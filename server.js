d481d75619b9f60d9920ea1e92fb64a50fe6ec67
ym987 <118288948+ym987@users.noreply.github.com>
2025-02-13 21:44:43 +0200

    add env.PORT

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const axios = require('axios');
 const app = express();
-const port = 8080;
+const port = process.env.PORT || 8080;
 const cors = require('cors');
 
 app.use(cors());
