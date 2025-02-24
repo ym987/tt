1a4a91b15c920583bee5082759c7c473405ba738
ym987 <118288948+ym987@users.noreply.github.com>
2025-02-24 21:21:24 +0200

    change baseURL to include process.env.API_URL

--- src/tt/axiosConfig.js
+++ src/tt/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'https://tt-kehilot.onrender.com', 
+  baseURL: process.env.API_URL || 'http://localhost:8080', // for production or local testing
   timeout: 1000,
   headers: { 'Content-Type': 'application/json' }
 });
