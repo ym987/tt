bce289039b23a3f15453fccf4490dfb1d87c2185
ym987 <118288948+ym987@users.noreply.github.com>
2025-02-26 15:29:09 +0200

    nee build

--- src/tt/axiosConfig.js
+++ src/tt/axiosConfig.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
+
+
 const axiosInstance = axios.create({
-  baseURL: process.env.API_URL || 'http://localhost:8080', // for production or local testing
+  baseURL: window.location.hostname === 'localhost' ? 'http://localhost:8080' : window.location.href, // for production or local testing
   // timeout: 1000,
   headers: { 'Content-Type': 'application/json' }
 });
