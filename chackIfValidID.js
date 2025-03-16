e556f7c5e4b236d135fe297137261507fc13b6da
ym987 <118288948+ym987@users.noreply.github.com>
2025-03-16 21:51:07 +0200

    add logs with chack valid ttID

---
+++ chackIfValidID.js
@@ -0,0 +1,24 @@
+const logs = require("./logs.json");
+const axios = require("axios");
+
+async function main() {
+  const validtt = [];
+  await Promise.all(logs.map(async (log) => {
+    const ttID = log.ttid;
+    if (await checkValidTT(ttID)) {
+      validtt.push(ttID);
+    }
+  }));
+  
+  console.log(validtt);
+  
+}
+
+main();
+
+async function checkValidTT(ttID) {
+  const apiUrl = "https://tt-s1kv.onrender.com/"; // For production on render
+  const response = await axios.post(apiUrl, { ttID });
+  // console.log("matching: ", response.data?.Matching);
+  return response.data?.Matching || false;
+}
